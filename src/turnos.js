3ee52aacfd700aaec9624ec9e5ffbdef92e2f840
JuanIPeretto <juaniperetto@gmail.com>
2026-06-28 11:48:47 -0300

    Progeso de login

--- src/turnos.js
+++ src/turnos.js
@@ -4,9 +4,11 @@ dotenv.config();
 import express from "express";
 import morgan from "morgan";
 import cors from "cors";
+import passport from "passport";
 
 import { testConexion } from "./db/test-conexion.js";
 import { manejarErrores } from "./middlewares/manejar-errores.js";
+import { estrategia, validacion} from './config/passport.js';
 
 import { router as v1EspecialidadesRutas } from "./rutas/especialidades-rutas.js";
 import { router as v1EstadisticasRutas } from "./rutas/estadisticas-rutas.js";
@@ -15,7 +17,7 @@ import { router as v1MedicosRutas } from "./rutas/medicos-rutas.js";
 import { router as v1PacientesRutas } from "./rutas/pacientes-rutas.js";
 import { router as v1TurnosReservas } from "./rutas/turnos-reservas-rutas.js";
 
-import authRutas from "./auth/auth-rutas.js";
+import { router as v1AuthRutas } from "./rutas/auth-rutas.js";
 import { swaggerUi, specs } from "./swagger.js";
 const app = express();
 
@@ -53,19 +55,30 @@ app.get('/', (req, res) => {
 
 /*
 |--------------------------------------------------------------------------
+| CONFIG PASSPORT
+|--------------------------------------------------------------------------
+*/
+
+passport.use(estrategia);
+passport.use(validacion);
+app.use(passport.initialize());
+
+/*
+|--------------------------------------------------------------------------
 | RUTAS API
 |--------------------------------------------------------------------------
 */
 
 // LOGIN JWT
-app.use('/api/v1', authRutas);
+app.use('/api/v1/auth', v1AuthRutas);
 
-app.use('/api/v1/especialidades', v1EspecialidadesRutas);
+app.use('/api/v1/especialidades', passport.authenticate('jwt', {session:false}), v1EspecialidadesRutas);
 app.use('/api/v1/estadisticas', v1EstadisticasRutas);
 app.use('/api/v1/obras-sociales', v1ObrasSocialesRutas);
 app.use('/api/v1/medicos', v1MedicosRutas);
 app.use('/api/v1/pacientes', v1PacientesRutas);
-app.use('/api/v1/turnos-reservas', v1TurnosReservas);
+app.use('/api/v1/turnos-reservas', passport.authenticate('jwt', {session:false}), v1TurnosReservas);
+
 
 /*
 |--------------------------------------------------------------------------
